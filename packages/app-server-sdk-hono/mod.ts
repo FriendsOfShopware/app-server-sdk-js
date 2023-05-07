f040c4d97fed880ffd5d542a85f90efaf9bbb7e7
Soner Sayakci <s.sayakci@shopware.com>
2023-05-07 22:36:10 +0200

    feat: add basic hono middleware

---
+++ packages/app-server-sdk-hono/mod.ts
@@ -0,0 +1,125 @@
+import { HTTPException } from "https://deno.land/x/hono@v3.1.8/http-exception.ts";
+import type {
+  Context as HonoContext,
+  Hono,
+} from "https://deno.land/x/hono@v3.1.8/mod.ts";
+import {
+  AppServer,
+  Context,
+  ShopInterface,
+  ShopRepositoryInterface,
+} from "https://deno.land/x/shopware_app_server_sdk@0.0.32/mod.ts";
+
+interface MiddlewareConfig {
+  appName: string;
+  appSecret: string;
+  appUrl?: string | null;
+  registrationUrl?: string | null;
+  registerConfirmationUrl?: string | null;
+  appPath?: string | null;
+  shopRepository:
+    | ShopRepositoryInterface
+    | ((c: HonoContext) => ShopRepositoryInterface);
+}
+
+type Variables = {
+  app: AppServer;
+  shop: ShopInterface;
+  context: Context;
+};
+
+let app: AppServer | null = null;
+
+export function configureAppServer(
+  hono: Hono<{ Variables: Variables }>,
+  cfg: MiddlewareConfig,
+) {
+  cfg.registrationUrl = cfg.registrationUrl || "/app/register";
+  cfg.registerConfirmationUrl = cfg.registerConfirmationUrl ||
+    "/app/register/confirm";
+  cfg.appPath = cfg.appPath || "/app/*";
+
+  hono.use(async (ctx, next) => {
+    if (app === null) {
+      const appUrl = cfg.appUrl ||
+        ctx.req.url.substring(
+          0,
+          ctx.req.url.length - (new URL(ctx.req.url).pathname.length),
+        );
+
+      if (typeof cfg.shopRepository === "function") {
+        // @ts-ignore
+        cfg.shopRepository = cfg.shopRepository(ctx);
+      }
+
+      app = new AppServer(
+        {
+          appName: cfg.appName,
+          appSecret: cfg.appSecret,
+          authorizeCallbackUrl: appUrl + cfg.registerConfirmationUrl,
+        },
+        cfg.shopRepository,
+      );
+    }
+
+    ctx.set("app", app);
+
+    await next();
+  });
+
+  hono.use(cfg.appPath, async (ctx, next) => {
+    const app = ctx.get("app") as AppServer;
+
+    let context;
+    try {
+      context = ctx.req.method === "GET"
+        ? await app.contextResolver.fromModule(ctx.req.raw)
+        : await app.contextResolver.fromSource(ctx.req.raw);
+    } catch (_e) {
+      throw new HTTPException(401, {
+        message: "Unauthorized",
+      });
+    }
+
+    ctx.set("shop", context.shop);
+    ctx.set("context", context);
+
+    await next();
+
+    const cloned = ctx.res.clone();
+
+    await ctx.get("app").signer.signResponse(
+      cloned,
+      ctx.get("shop").getShopSecret(),
+    );
+
+    ctx.header(
+      "shopware-app-signature",
+      cloned.headers.get("shopware-app-signature") as string,
+    );
+  });
+
+  hono.get(cfg.registrationUrl, async (ctx) => {
+    const app = ctx.get("app") as AppServer;
+
+    try {
+      return await app.registration.authorize(ctx.req.raw);
+    } catch (_e) {
+      throw new HTTPException(400, {
+        message: "Invalid request",
+      });
+    }
+  });
+
+  hono.post(cfg.registerConfirmationUrl, async (ctx) => {
+    const app = ctx.get("app") as AppServer;
+
+    try {
+      return await app.registration.authorizeCallback(ctx.req.raw);
+    } catch (_e) {
+      throw new HTTPException(400, {
+        message: "Invalid request",
+      });
+    }
+  });
+}
---
+++ packages/app-server-sdk-hono/scripts/build.ts
@@ -0,0 +1,54 @@
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  test: false,
+  scriptModule: false,
+  shims: {
+    deno: false,
+    blob: false,
+    crypto: false,
+    domException: false,
+    prompts: false,
+    timers: false,
+    undici: false,
+    weakRef: false,
+    webSocket: false,
+  },
+  mappings: {
+    "https://deno.land/x/hono@v3.1.8/mod.ts": {
+      name: "hono",
+      version: "^3.1.8",
+      peerDependency: false,
+    },
+    "https://deno.land/x/hono@v3.1.8/http-exception.ts": {
+      name: "hono",
+      version: "^3.1.8",
+      subPath: "http-exception",
+    },
+    "https://deno.land/x/shopware_app_server_sdk@0.0.32/mod.ts": {
+      name: "@friendsofshopware/app-server-sdk",
+      version: "^0.0.32",
+      peerDependency: false,
+    },
+  },
+  compilerOptions: {
+    lib: ["es2022", "dom", "webworker"],
+  },
+  package: {
+    name: "@friendsofshopware/app-server-sdk-hono",
+    version: Deno.args[0],
+    description: "Hono Integration for Shopware App Server SDK",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/FriendsOfShopware/app-server-sdk-js.git",
+    },
+    bugs: {
+      url: "https://github.com/FriendsOfShopware/app-server-sdk-js/issues",
+    },
+  }
+});
