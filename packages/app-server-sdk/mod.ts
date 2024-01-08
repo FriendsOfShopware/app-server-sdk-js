d3c18313adb1cae4afc009d52ef5b3795ff2e4ee
Shyim <s.sayakci@gmail.com>
2024-01-08 20:26:10 +0100

    export api errors

--- packages/app-server-sdk/mod.ts
+++ packages/app-server-sdk/mod.ts
@@ -2,6 +2,6 @@ export { AppServer } from "./app.ts";
 export type { AppConfigurationInterface } from "./app.ts";
 export { InMemoryShopRepository, SimpleShop, DenoKVRepository } from "./repository.ts"
 export type { ShopInterface, ShopRepositoryInterface } from "./repository.ts"
-export { HttpClient, HttpClientResponse } from "./http-client.ts"
+export { HttpClient, HttpClientResponse, ApiClientAuthenticationFailed, ApiClientRequestFailed } from "./http-client.ts"
 export { WebCryptoHmacSigner } from "./signer.ts"
 export { Context } from "./context-resolver.ts"
